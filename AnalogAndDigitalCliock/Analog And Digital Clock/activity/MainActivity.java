1f8fe2d038652ffcc3cb0faf0b713fccaaa780ad
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-03-02 21:21:40 +0530

    Analog and Digital Clock
    see application output

---
+++ AnalogAndDigitalCliock/Analog And Digital Clock/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.checkboxwithtoast;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Analog and Digital Clock");
+    }
+}
