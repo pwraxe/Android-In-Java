35831db6e67b799b30b68d4c96e116e259518196
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-03-03 11:00:38 +0530

    Radio Button
    see application output

---
+++ app_radiobutton/RadioButtonDemo/activity/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.radiobuttondemo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    RadioGroup Rgrp;
+    Button submit;
+    RadioButton Rbutton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Rgrp = findViewById(R.id.radioGrp);
+        submit = findViewById(R.id.submit_button);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int getId = Rgrp.getCheckedRadioButtonId();
+                Rbutton = findViewById(getId);
+                Toast.makeText(MainActivity.this,Rbutton.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
