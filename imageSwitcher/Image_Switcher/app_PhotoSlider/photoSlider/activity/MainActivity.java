5a0c48d423e90319da812ca4646b73cbe5f1b736
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-03-03 10:59:03 +0530

    Photo slide vertically and Horizontaly
    see application output

---
+++ imageSwitcher/Image_Switcher/app_PhotoSlider/photoSlider/activity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.photoslider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.butt);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
